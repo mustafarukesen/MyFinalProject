e3bfa33579be2ad56f3980726047f0d1f9cf1910
Mustafa Faruk Esen <mustafarukesen@gmail.com>
2021-02-25 00:08:05 +0300

    24 Şubat

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz!";
         public static string MaintenanceTime = "Sistem bakımda!";
         public static string ProductsListed = "Ürünler listelendi!";
+        public static string ProductCountCategoryError = "Bir categoryden en fazla 10 adet yapabilirsiniz!";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten başka bir ürün var!";
+        public static string CategoryLimitExceded = "Kategori sayısı 15 den fazla olduğu için yeni ürün ekleyemezsiniz!";
     }
 }
